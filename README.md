0cf223b842fd5143048e0e52dbef14d3b4fbd677
Jonathan V. Solórzano <JonathanVSV@users.noreply.github.com>
2020-08-07 20:01:31 -0500

    Added AccuracyClass

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This repository contains the data and scripts used to identify differences between the structural attributes of conserved and degraded tropical dry forest in the Ayuquila River watershed, Mexico. 
 
-The scripts and data can be found inside the Scripts folder. The Mann-Whitney test and logistic regression procedure can be consulted in the Wilcox_LogReg.R file, while the correlations analysis can be consulted in the Correlations.R file. In turn, the scripts used to construct the boxplots and barplots are available in the Boxplots.R and Barplots.R. Finally, the data used to perform all the analyses is available in the plots_data.csv file.
+The scripts and data can be found inside the Scripts folder. The Mann-Whitney test and logistic regression procedure can be consulted in the Wilcox_LogReg.R file, while the correlations analysis can be consulted in the Correlations.R file. The script used for calculating the accuracy of each structural variable for classifying the plots into degraded and conserved is available in AccuracyClass.R file. In turn, the scripts used to construct the boxplots and barplots are available in the Boxplots.R and Barplots.R. Finally, the data used to perform all the analyses is available in the plots_data.csv file.
 
 To download all the files click on the green button "Code" and download as zip or open with Github Desktop App.
 
---
+++ Scripts/AccuracyClass.R
@@ -0,0 +1,48 @@
+library(tidyverse)
+library(caret)
+
+# Read and prepare data------------------------------------
+
+df <- read.csv("plots_data.csv", stringsAsFactors = T)
+
+## Change data form wide to long format
+df_long <- df %>%
+  # Indicate column names to use
+  select(Sitio, tipo_bosque,Cobertura, AB_m2.ha, Media_altura, biomasa_mg.ha, Media_altura, Total_ramas, arboles.ha) %>%
+  # Prepare data: long format. Indicate which columns are going into long format
+  pivot_longer(cols = c(Cobertura, AB_m2.ha, Media_altura, biomasa_mg.ha, Media_altura, Total_ramas, arboles.ha),
+               names_to = "Attribute",
+               values_to = "value")
+
+# Calculate Gini Index---------------------------------------
+
+## Define nesting function
+## Taken from: https://github.com/tidyverse/tidyr/issues/769#issuecomment-537624093
+func_nest <- function(df, vars_to_nest, new_col) {
+  nest(df, !!new_col := {{ vars_to_nest }})
+}
+
+resul <- df_long %>%
+  # Remove columns that are not going to be used
+  select(-Sitio) %>%
+  # Use func_nest to nest the data for each attribute
+  func_nest(-Attribute, "data_nest") %>%
+  # Get the glm fit, and get the fitted values
+  mutate(fit = map(data_nest, ~ glm(.$tipo_bosque ~ .$value, family = "binomial")$fitted.values),
+         fitted_vals = map(fit, function(x) as.data.frame(x)),
+         real_vals = map(data_nest, function(x) as.data.frame(x$tipo_bosque))) %>%
+  # unnest fitted and real values
+  unnest(c(fitted_vals,real_vals)) %>%
+  # Rename unnested columnes
+  rename("fitted_vals2" = "x", "true" = "x$tipo_bosque") %>%
+  # Transfrom binomial probs into factor levels
+  mutate(fitted = ifelse(fitted_vals2 >= 0.5, "degraded", "conserved")) %>%
+  # Eliminate columns that we are not going to use
+  select(c(-data_nest, -fit, -fitted_vals2)) %>%
+  # Set columns as factors
+  mutate_at(vars(fitted, true), function(x) as.factor(x)) %>%
+  # Nesta data 
+  func_nest(-Attribute, "data_nest_fit") %>%
+  # Calculate confusion matrix accuracy
+  mutate(acc = unlist(map(data_nest_fit, ~ confusionMatrix(.$true,.$fitted)$overall[1]*100)))
+
